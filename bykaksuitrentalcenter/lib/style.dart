6c90bfd2cfc02a4c7cd4be980e428e74ffffc876
this-is-yello <yello_3617@naver.com>
2023-01-19 01:18:20 +0900

    230119 in Home

---
+++ bykaksuitrentalcenter/lib/style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+var mainColor = Color(0xFF205B48);
+var blackColor = Color(0xFF1E1E1E);
+var greyColor = Color(0xFF9E9E9E);
+var lightGreyColor = Color(0xFFEEEEEE);
+
+var theme = ThemeData(
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    actionsIconTheme: IconThemeData(color: blackColor),
+    iconTheme: IconThemeData(color: blackColor),
+    toolbarHeight: 72,
+  )
+);
